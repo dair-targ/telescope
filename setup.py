79f392e1c44787c6b5ec111447495eaf956036d8
Vladimir Berkutov <vladimir.berkutov@gmail.com>
2016-04-12 21:53:00 +0900

    Add basic tests

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-  name='Telescope',
-  version='1',
-  packages=find_packages(),
-  url='',
-  license='',
-  author='Vladimir Berkutov',
-  author_email='vladimir.berkutov@gmail.com',
-  description='',
+    name='Telescope',
+    version='1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Vladimir Berkutov',
+    author_email='vladimir.berkutov@gmail.com',
+    description='',
 
-  install_requires=[
-    'pyserial==3.0.1',
-  ]
+    install_requires=[
+        'pyserial==3.0.1',
+        'pytest==2.9.1',
+    ]
 )
